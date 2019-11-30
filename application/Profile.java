648c6209c948be9e62c5ddda287f515a9a119d24
charasi <asiama@wisc.edu>
2019-11-30 13:51:25 -0600

    Updated Profile class to set and get usernames, update friends list, determine center position, and set users picture

--- application/Profile.java
+++ application/Profile.java
@@ -4,30 +4,77 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Profile {
-  String userName;
-  List<Profile> friends;
-  public Profile(String userName) {
-    this.userName = userName;
-    friends = new LinkedList<Profile>();
-  }
-  public void testInit() {
-    friends.add(new Profile("mark"));
-    friends.add(new Profile("sapan"));
-    friends.add(new Profile("deb"));
-    friends.add(new Profile("john"));
-  }
-  public List<Profile> getFriends(){
-    return friends;
-  }
-  public String getUserName() {
-    return userName;
-  }
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-  public void setFriends(List<Profile> friends) {
-    this.friends = friends;
-  }
+	String user_Name; //user name
+	String user_Picture;
+	boolean is_User_Center = false;
+	List<List<Profile>> list_of_user_friends; //list of users friends
   
+	/**
+	* Constructor to set users name and create a list of users friends
+	* @param userName Users name.
+	*/
+	public Profile(String userName) {
+		this.user_Name = userName;
+		this.list_of_user_friends = new LinkedList<List<Profile>>();
+	}
   
+	/**
+	 * @return list of users friends
+	 */
+	public List<List<Profile>> getListOfUsersFriends(){
+		return this.list_of_user_friends;
+	}	
+	
+	/**
+	 * @return user name
+	 */
+	public String getUserName() {
+	  return this.user_Name;
+	}
+	
+	/**
+	 * @return user name
+	 */
+	public String getUserPicture() {
+	  return this.user_Picture;
+	}
+	
+	/**
+	 * Sets users picture
+	 */
+	public void setUserPicture(String userPicture) {
+	  this.user_Picture = userPicture;
+	}
+	
+	/**
+	 * Sets user name
+	 * @param userName
+	 */
+	public void setUserName(String userName) {
+	    this.user_Name = userName;
+	}
+	  
+	/**
+	 * Adds users friends and all of its friends
+	 * @param friends
+	 */
+	public void setFriends(List<Profile> friends) {
+	   this.list_of_user_friends.add(friends);
+	}
+	
+	/**
+	 * Sets users center position to true or false to determine if user is in center position
+	 * @param isUserCenter
+	 */
+	public void setUserCenter(boolean isUserCenter) {
+	    this.is_User_Center = isUserCenter;
+	}
+	
+	/**
+	 * Returns true or false to determine if a user is in the center position
+	 * @return is_User_Center
+	 */
+	public boolean getUserCenter() {
+	    return this.is_User_Center;
+	}
 }
