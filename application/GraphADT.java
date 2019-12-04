90a7ce0136d1064f134a28b28986210a057e7f72
charasi <asiama@wisc.edu>
2019-12-03 19:28:00 -0600

    GraphADT update

--- application/GraphADT.java
+++ application/GraphADT.java
@@ -5,4 +5,11 @@ public interface GraphADT<T> {
   boolean addFriend(T user, T friend);
   boolean deleteUser(T user);
   boolean deleteFriend(T user, T friend);
+  boolean deleteUser(String profile);
+  boolean addUser(String profile);
+boolean addFriend(String profileA, String profileB);
+boolean clearNetwork();
+int order();
+boolean removeFriend(String profileA, String profileB);
+int size();
 }
