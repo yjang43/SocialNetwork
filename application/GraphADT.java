16aa826b0376360f2d83c5b7d1ed02689c048059
JANG <jang@JANGs-MacBook-Air.local>
2019-12-06 22:19:38 -0600

    More changes before push for Main.java Backup
    Still some erro in Graph.java and GraphADT.java
    Fixed those

--- application/GraphADT.java
+++ application/GraphADT.java
@@ -2,14 +2,18 @@ package application;
 
 public interface GraphADT<Profile> {
   boolean addUser(Profile user);
+
   boolean addFriend(Profile user, Profile friend);
+
   boolean deleteUser(Profile user);
+
   boolean deleteFriend(Profile user, Profile friend);
-  boolean deleteUser(Profile profile);
-  boolean addUser(Profile profile);
-boolean addFriend(Profile profileA, Profile profileB);
-boolean clearNetwork();
-int order();
-boolean removeFriend(Profile profileA, Profile profileB);
-int size();
+
+  boolean clearNetwork();
+
+  int order();
+
+  boolean removeFriend(Profile profileA, Profile profileB);
+
+  int size();
 }
