6a20812a0d9f62c94fd4e258d4a7570a25480a01
Dana Ingram <danaingram@Danas-MacBook-Pro.local>
2019-12-08 23:25:46 -0600

    Updated Graph
    Minor changes to methods in graph class

--- application/Graph.java
+++ application/Graph.java
@@ -17,42 +17,41 @@ public class Graph implements GraphADT<Profile> {
 
 	private int numOfUser = 0;
 	private int numOfEdges = 0;
-	
-	private Map< Profile,ArrayList<Profile>> myMap;
+
+	private Map<Profile, ArrayList<Profile>> myMap;
 	private ArrayList<Profile> vertexList;
-	
+
 	/*
 	 * Default no-argument constructor
-	 */ 
+	 */
 	public Graph() {
 		vertexList = new ArrayList<>();
-	    myMap = new HashMap<Profile,ArrayList<Profile>>(); 
+		myMap = new HashMap<Profile, ArrayList<Profile>>();
 	}
-	
-    /**
-     * Add new user to the graph.
-     *
-     * If user is null return false
-     * 
-     *  
-     * If user is already exists return true
-     * and without adding a vertex or 
-     * throwing an exception.
-     * 
-     * Valid argument conditions:
-     * 1. user is non-null
-     * 2. user is not already in the graph 
-     * 
-     * @param profile the user to be added
-     * @return true if addUser, otherwise false
-     */
+
+	/**
+	 * Add new user to the graph.
+	 *
+	 * If user is null return false
+	 * 
+	 * 
+	 * If user is already exists return true and without adding a vertex or throwing
+	 * an exception.
+	 * 
+	 * Valid argument conditions: 1. user is non-null 2. user is not already in the
+	 * graph
+	 * 
+	 * @param profile the user to be added
+	 * @return true if addUser, otherwise false
+	 */
 	@Override
 	public boolean addUser(Profile profile) {
-		if (profile==null) return false;
-				
-		else if (myMap.containsKey(profile))return true;
-		
-		
+		if (profile == null)
+			return false;
+
+		else if (myMap.containsKey(profile))
+			return true;
+
 		// if user is not null and not already in the graph
 		else {
 			myMap.put(profile, new ArrayList<Profile>());
@@ -63,120 +62,117 @@ public class Graph implements GraphADT<Profile> {
 	}
 
 	/**
-	 * Remove a user and all associated 
-	 * edges from the graph.
+	 * Remove a user and all associated edges from the graph.
 	 * 
-	 * If user is null or does not exist,
-	 * method ends without removing a user, edges, 
-	 * or throwing an exception. and return false.
+	 * If user is null or does not exist, method ends without removing a user,
+	 * edges, or throwing an exception. and return false.
 	 * 
-	 * Valid argument conditions:
-	 * 1. user is non-null
-	 * 2. user is not already in the graph 
-	 *  
+	 * Valid argument conditions: 1. user is non-null 2. user is not already in the
+	 * graph
+	 * 
 	 * @param profile the user to be removed
 	 * @return true if delete friend, otherwise false
 	 */
 	@Override
 	public boolean deleteUser(Profile profile) {
-		if(profile==null||!myMap.containsKey(profile))return false;
-		
+		if (profile == null || !myMap.containsKey(profile))
+			return false;
+
 		else {
 			myMap.remove(profile);
 			numOfUser--;
 			return true;
 		}
-		
+
 	}
+
 	/**
-	 * Add the user from profileA to profileB
-	 * to this graph.  (edge is directed and weighted)
+	 * Add the user from profileA to profileB to this graph. (edge is directed and
+	 * weighted)
 	 * 
-	 * If either user does not exist,
-	 * VERTEX IS ADDED and then edge is created.
-	 * No exception is thrown.
+	 * If either user does not exist, VERTEX IS ADDED and then edge is created. No
+	 * exception is thrown.
 	 *
-	 * If the edge exists in the graph,
-	 * no edge is added and no exception is thrown.
+	 * If the edge exists in the graph, no edge is added and no exception is thrown.
 	 * 
-	 * Valid argument conditions:
-	 * 1. neither user is null
-	 * 2. both users are in the graph 
-	 * 3. the edge is not in the graph
-	 *  
+	 * Valid argument conditions: 1. neither user is null 2. both users are in the
+	 * graph 3. the edge is not in the graph
+	 * 
 	 * @param profileA the first user (src)
 	 * @param profileB the second user (dst)
-	 * @return true if successful adding a friend, otherwise false 
+	 * @return true if successful adding a friend, otherwise false
 	 */
 	@Override
 	public boolean addFriend(Profile profileA, Profile profileB) {
 		boolean success = false;
-		if(profileA!=null&&profileB!=null) {
+		if (profileA != null && profileB != null) {
 			// condition 1:
 			// if both vertex doesn't exist
 			// add vertex and add edge
-			if(!myMap.containsKey(profileA)&&!myMap.containsKey(profileB)) {
+			if (!myMap.containsKey(profileA) && !myMap.containsKey(profileB)) {
 				// add vertex
 				addUser(profileA);
 				addUser(profileB);
+				// adds each user to the other's friend list
+				profileA.list_of_user_friends.add(profileB);
+				profileB.list_of_user_friends.add(profileA);
 				// add edge
-				ArrayList<Profile> container = myMap.get(profileA);
-				container.add(profileB);
 				numOfEdges++;
 				success = true;
 			}
 			// condition2:
 			// if vertex1 exist, but vertex2 not
-			if(myMap.containsKey(profileA)&&!myMap.containsKey(profileB)) {
+			else if (myMap.containsKey(profileA) && !myMap.containsKey(profileB)) {
 				addUser(profileB);
+				// adds each user to the other's friend list
+				profileA.list_of_user_friends.add(profileB);
+				profileB.list_of_user_friends.add(profileA);
 				// add edge
-				ArrayList<Profile> container = myMap.get(profileA);
-				container.add(profileB);
 				numOfEdges++;
 				success = true;
 			}
 			// condition3:
 			// if vertex2 exist, but vertex1 not
-			if(!myMap.containsKey(profileA)&&myMap.containsKey(profileB)) {
+			else if (!myMap.containsKey(profileA) && myMap.containsKey(profileB)) {
 				addUser(profileA);
+				// adds each user to the other's friend list
+				profileA.list_of_user_friends.add(profileB);
+				profileB.list_of_user_friends.add(profileA);
 				// add edge
-				ArrayList<Profile> container = myMap.get(profileA);
-				container.add(profileB);
 				numOfEdges++;
 				success = true;
 			}
-			//condition4:
+			// condition4:
 			// if contain both key in the graph
 			// no edge in the graph
-			else if(myMap.containsKey(profileA)&&myMap.containsKey(profileB)) {
+			else {
 				// check if already has the edge
-				if(myMap.get(profileA).contains(profileB)) {
+				if (profileA.list_of_user_friends.contains(profileB)) {
 					success = false;
-					}
+				}
 				// if no edge exist
 				else {
-					ArrayList<Profile> container = myMap.get(profileA);
-					container.add(profileB);
+					// adds each user to the other's friend list
+					profileA.list_of_user_friends.add(profileB);
+					profileB.list_of_user_friends.add(profileA);
 					numOfEdges++;
 					success = true;
 				}
 			}
-		}return success;
+		}
+		return success;
 	}
-	
+
 	/**
-	 * Remove the edge from profileA to profileB
-	 * from this graph.  (edge is directed and weighted)
-	 * If either user does not exist,
-	 * or if an edge from profileA to profileB does not exist,
-	 * return false no edge is removed and no exception is thrown.
+	 * Remove the edge from profileA to profileB from this graph. (edge is directed
+	 * and weighted) If either user does not exist, or if an edge from profileA to
+	 * profileB does not exist, return false no edge is removed and no exception is
+	 * thrown.
 	 * 
 	 * 
-	 * Valid argument conditions:
-	 * 1. neither vertex is null
-	 * 2. both vertices are in the graph 
-	 * 3. the edge from profileA to profileB is in the graph
-	 *  
+	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are in
+	 * the graph 3. the edge from profileA to profileB is in the graph
+	 * 
 	 * @param profileA the first user
 	 * @param profileB the second user
 	 * @return return false if vertex doesn't exist
@@ -184,48 +180,54 @@ public class Graph implements GraphADT<Profile> {
 	@Override
 	public boolean deleteFriend(Profile profileA, Profile profileB) {
 		boolean success = false;
-		//condition1: the vertex doesn't exist
-		if(!myMap.containsKey(profileA)||!myMap.containsKey(profileB)) {
+		// condition1: the vertex doesn't exist
+		if (!myMap.containsKey(profileA) || !myMap.containsKey(profileB)) {
 			success = false;
 			// the vertex are exist
-		}else {
-			//condition2: the edge is not exist
-			if(!myMap.get(profileA).contains(profileB)) {success = false;}
-			
-			//codition3: the edge is exist, remove it.
-			if(myMap.get(profileA).contains(profileB)) {
-				myMap.get(profileA).remove(profileB);		
+		} else {
+			// condition2: the edge is not exist
+			if (!profileA.list_of_user_friends.contains(profileB)) {
+				success = false;
+			}
+			// codition3: the edge is exist, remove it.
+			else if (profileA.list_of_user_friends.contains(profileB)) {
+				profileA.list_of_user_friends.remove(profileB);
+				profileB.list_of_user_friends.remove(profileA);
 				numOfEdges--;
-				success = true;;
+				success = true;
 			}
 		}
 		return success;
 	}
 
-
 	/**
 	 * Deleting all the users and relations
-	 *  
+	 * 
 	 * if no user and relationship in the graph return true
 	 *
-	 * @param boolean if delete successfully return true
-	 *  otherwise return false
+	 * @param boolean if delete successfully return true otherwise return false
 	 */
 
 	@Override
 	public boolean clearNetwork() {
-		myMap =  new HashMap<Profile,ArrayList<Profile>>();
-		vertexList =  new ArrayList<>();
+		myMap = new HashMap<Profile, ArrayList<Profile>>();
+		vertexList = new ArrayList<>();
 		return true;
-		
+
 	}
-	
+
+	/**
+	 * Returns the list of vertexes
+	 * 
+	 * @return
+	 */
 	public ArrayList<Profile> getVertexList() {
 		return vertexList;
 	}
 
 	/**
 	 * Returns the number of edges in this graph.
+	 * 
 	 * @return number of edges in the graph.
 	 */
 	@Override
@@ -235,6 +237,7 @@ public class Graph implements GraphADT<Profile> {
 
 	/**
 	 * Returns the number of vertices in this graph.
+	 * 
 	 * @return number of vertices in graph.
 	 */
 	@Override
@@ -249,6 +252,5 @@ public class Graph implements GraphADT<Profile> {
 		// TODO Auto-generated method stub
 
 	}
-
 
 }
